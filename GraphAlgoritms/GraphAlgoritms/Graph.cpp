15a0d6dac7b7536e45fe607563ae01557a9153fc
KseniaEnot <Dogz50@mial.ru>
2020-11-26 18:06:57 +0300

    .cpp

--- GraphAlgoritms/GraphAlgoritms/Graph.cpp
+++ GraphAlgoritms/GraphAlgoritms/Graph.cpp
@@ -1,1 +1,43 @@
 #include "Graph.h"
+#include <iostream>
+#include <stdlib.h>
+
+void Graph::initGraph() {
+
+}
+
+bool Graph::IfEulerian() {
+
+}
+
+bool Graph::IfBipartite() {
+
+}
+
+bool Graph::IfTree() {
+	
+}
+
+int* Graph::PruferCode() {
+
+}
+
+void Graph::PruferDecode(int* PrufC) {
+
+}
+
+void Graph::StrongConnected() {
+
+}
+
+int* Graph::Dijkstra(int A, int B) {
+
+}
+
+Iterator* Graph::create_dft_iterator() {
+	//return new dft_Iterator(/**/);
+}
+
+Iterator* Graph::create_bft_iterator() {
+	//return new dft_Iterator(/**/);
+}
