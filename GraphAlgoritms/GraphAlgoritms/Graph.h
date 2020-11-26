f2c5a01cac44c33cea78a2b8bada0823b4b0ca8b
KseniaEnot <Dogz50@mial.ru>
2020-11-26 20:45:15 +0300

    dft_1

--- GraphAlgoritms/GraphAlgoritms/Graph.h
+++ GraphAlgoritms/GraphAlgoritms/Graph.h
@@ -1,5 +1,6 @@
 #pragma once
-#include "Iterator.h"
+#include "Iterator.h";
+#include "dualList.h"
 #include <stdlib.h>
 #include <iostream>
 
@@ -32,6 +33,34 @@ public:
 	int* Dijkstra(int,int);
 	Iterator* create_dft_iterator(); // depth-first traverse iterator
 	Iterator* create_bft_iterator(); // breadth-first traverse iterator
+
+	class dft_Iterator : public Iterator // depth-first traverse
+	{
+	private:
+		bool* visited;
+		int** ItrG;
+		dualList* Stack;
+		int Icurrent;
+		int sizeV;
+	public:
+		dft_Iterator(int** Gr, int max) {
+			sizeV = max;
+			ItrG = Gr;
+			Icurrent = 0;
+			visited = new bool[max];
+			visited[0] = true;
+			Stack->push_back(Icurrent);
+			for (size_t i = 1; i < max; i++)
+				visited[i] = false;
+		};
+		int next();
+		bool has_next();
+		~dft_Iterator() {
+			delete visited;
+			delete* ItrG; //??norm
+		}
+	};
+
 
 	~Graph() {
 		for (size_t i = 0; i < V; i++)
